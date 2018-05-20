8e81321116d41e251f607342ec4705ac67aacd24
sethoeni <s.thoeni@gmx.ch>
2018-05-20 09:12:13 +0000

    Add scores table

--- backend/sql/dbsetup.sql
+++ backend/sql/dbsetup.sql
@@ -18,13 +18,18 @@ CREATE TABLE IF NOT EXISTS siegerbuch_prod.plays	(
 	play_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	game_id INT UNSIGNED,
 	played_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	players TEXT,
-	scores TEXT,
 	comment TEXT DEFAULT NULL,
-	FOREIGN KEY (game_id) REFERENCES siegerbuch_prod.games(game_id),
-	FOREIGN KEY (winner_id) REFERENCES siegerbuch_prod.players(player_id)
+	FOREIGN KEY (game_id) REFERENCES siegerbuch_prod.games(game_id)
 )ENGINE=InnoDB;
 
+CREATE TABLE IF NOT EXISTS siegerbuch_prod.scores (
+	play_id INT UNSIGNED,
+	player_id INT UNSIGNED,
+	score INT,
+	PRIMARY KEY (play_id, player_id),
+	FOREIGN KEY (play_id) REFERENCES siegerbuch_prod.plays(play_id),
+	FOREIGN KEY (player_id) REFERENCES siegerbuch_prod.players(player_id)
+)ENGINE=InnoDB;
 
 CREATE DATABASE IF NOT EXISTS siegerbuch_test;
 
@@ -45,12 +50,18 @@ CREATE TABLE IF NOT EXISTS siegerbuch_test.players (
 CREATE TABLE IF NOT EXISTS siegerbuch_test.plays	(
 	play_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	game_id INT UNSIGNED,
-	played_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-	players TEXT,
-	scores TEXT,
+	played_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	comment TEXT DEFAULT NULL,
-	FOREIGN KEY (game_id) REFERENCES siegerbuch_test.games(game_id),
-	FOREIGN KEY (winner_id) REFERENCES siegerbuch_test.players(player_id)
+	FOREIGN KEY (game_id) REFERENCES siegerbuch_test.games(game_id)
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS siegerbuch_test.scores (
+	play_id INT UNSIGNED,
+	player_id INT UNSIGNED,
+	score INT,
+	PRIMARY KEY (play_id, player_id),
+	FOREIGN KEY (play_id) REFERENCES siegerbuch_test.plays(play_id),
+	FOREIGN KEY (player_id) REFERENCES siegerbuch_test.players(player_id)
 )ENGINE=InnoDB;
 
 CREATE USER siegerb_scribe IDENTIFIED BY 'siegerbuch_Scribe_password';
